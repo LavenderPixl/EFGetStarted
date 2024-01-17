9cabae4a2a8edf0d16e2c2cbc7016cab647a8cbf
LavenderPixl <annajacobsen1999@hotmail.com>
2024-01-17 13:42:15 +0100

    Assignment - 2.2

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -3,52 +3,54 @@ using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Quantum.QsCompiler.CompilationBuilder;
+using System.Text.Json;
 
 
 using var db = new BloggingContext();
 
-// Note: This sample requires the database to be created before running.
-Console.WriteLine($"Database path: {db.DbPath}.");
+//// Note: This sample requires the database to be created before running.
+//Console.WriteLine($"Database path: {db.DbPath}.");
 
-// Create
-Console.WriteLine("Inserting a new blog");
-db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-db.SaveChanges();
+//// Create
+//Console.WriteLine("Inserting a new blog");
+//db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+//db.SaveChanges();
 
-// Read
-Console.WriteLine("Querying for a blog");
-var blog = db.Blogs
-    .OrderBy(b => b.BlogId)
-    .First();
+//// Read
+//Console.WriteLine("Querying for a blog");
+//var blog = db.Blogs
+//    .OrderBy(b => b.BlogId)
+//    .First();
 
-// Update
-Console.WriteLine("Updating the blog and adding a post");
-blog.Url = "https://devblogs.microsoft.com/dotnet";
-blog.Posts.Add(
-    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-db.SaveChanges();
+//// Update
+//Console.WriteLine("Updating the blog and adding a post");
+//blog.Url = "https://devblogs.microsoft.com/dotnet";
+//blog.Posts.Add(
+//    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+//db.SaveChanges();
 
-// Delete
-Console.WriteLine("Delete the blog");
-db.Remove(blog);
-db.SaveChanges();
+//// Delete
+//Console.WriteLine("Delete the blog");
+//db.Remove(blog);
+//db.SaveChanges();
+
+printIncompleteTasksAndTodos();
 //seedTasks();
 
-using (BloggingContext context = new())
-{
-    var tasks = context.Tasks.Include(task => task.Todos);
-    foreach (var task in tasks)
-    {
-        Console.WriteLine($"Task: {task.Name}");
-        foreach (var todo in task.Todos)
-        {
-            Console.WriteLine($"- {todo.Name}"); 
-        }
-    }
-}
+//using (BloggingContext context = new())
+//{
+//    var tasks = context.Tasks.Include(task => task.Todos);
+//    foreach (var task in tasks)
+//    {
+//        Console.WriteLine($"Task: {task.Name}");
+//        foreach (var todo in task.Todos)
+//        {
+//            Console.WriteLine($"- {todo.Name}");
+//        }
+//    }
+//}
 
-
-    static void seedTasks()
+static void seedTasks()
 {
     List<Todo> List1 = new List<Todo>
     {
@@ -66,10 +68,30 @@ using (BloggingContext context = new())
     };
     Tasks T2 = new Tasks(2, "Brew coffee", List2) { };
 
-    using(BloggingContext context = new())
+    using (BloggingContext context = new())
     {
         context.Tasks.Add(T1);
         context.Tasks.Add(T2);
         context.SaveChanges();
+    }
+}
+static void printIncompleteTasksAndTodos()
+{
+    using (var context = new BloggingContext())
+    {
+        var tasks = context.Tasks.Include(task => task.Todos.Where(p => p.IsComplete == false));
+        foreach (var task in tasks)
+        {
+            Console.WriteLine($"Uncompleted Tasks:  \nTask: {task.Name}");
+            foreach (var todo in task.Todos)
+            {
+                if (todo.IsComplete == false)
+                {
+                    Console.WriteLine($"- {todo.Name} - {todo.IsComplete}");
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
     }
 }
